2334253062225cdfbeb921f059155d9a8cded882
sabrismd <syedsabri3333@gmail.com>
2024-11-19 18:47:48 +0530

    changed

--- trackerproj/settings.py
+++ trackerproj/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import json as js
 import os
-env = js.load(open(os.path.join(os.getcwd(),'config/config.json')))
+env = js.load(open('config/config.json'))
 env = env['environment']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
